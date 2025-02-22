6f6fe83a569218aaa15de9dd5a5254d7033c857e
Martina ILko <ilkomartina12@gmail.com>
2025-02-22 23:45:32 +0100

    Vytvoreni tridy Mistnost

---
+++ src/Mistnost.java
@@ -0,0 +1,12 @@
+public class Mistnost {
+    private String name;
+
+    public Mistnost(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "\nMistnost: " + name;
+    }
+}
