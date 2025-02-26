6bd31275d936f3cf102660fad6b5b583686b4f06
Martina ILko <ilkomartina12@gmail.com>
2025-02-26 20:54:00 +0100

    Opraveno načítání mapy v Svet, pridano par metod

---
+++ src/svet/Svet.java
@@ -0,0 +1,46 @@
+package svet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Svet {
+   public static HashMap<Mistnost, ArrayList<Mistnost>> svet = new HashMap<>();
+
+    public Svet(){
+        nacteniMapy();
+    }
+
+    private void nacteniMapy() {
+        try (BufferedReader br = new BufferedReader(new FileReader("mapa.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] mistnosti = line.split(" ");
+                Mistnost mistnost = new Mistnost(mistnosti[0]);
+                ArrayList<Mistnost> mistnostiVedle = new ArrayList<>();
+                for (int i = 1; i < mistnosti.length; i++) {
+                    mistnostiVedle.add(new Mistnost(mistnosti[i]));
+                }
+                svet.put(mistnost,mistnostiVedle);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public HashMap<Mistnost, ArrayList<Mistnost>> getSvet() {
+        return svet;
+    }*/
+
+    public ArrayList<Mistnost> moznostPohybu(Mistnost mistnost) {
+        return svet.get(mistnost);
+    }
+
+    public void printMap(){
+        for(Map.Entry<Mistnost,ArrayList<Mistnost>> entry : svet.entrySet()){
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
